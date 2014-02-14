b212a456c0e1ae8d24f47a4971e1894bea4004f4
balack.xu <balack.xu@b5m.com>
2014-02-14 14:48:59 +0800

    upload2

--- update.sql
+++ update.sql
@@ -7,5 +7,7 @@
  * To change this template use File | Settings | File Templates.
  */
 alter table zhi_post add slogan varchar(255) not null default '';
+alter table zhi_mall add email varchar(255) not null default '';
+alter table zhi_mall add tel varchar(255) not null default '';
+alter table zhi_mall add addr varchar(255) not null default '';
 
-delete from zhi_post_cate where id>68;
